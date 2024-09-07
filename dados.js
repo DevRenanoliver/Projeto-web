c14c87260076fe08cd05c98db8cacda44c017c0a
Renan Oliveira <devrenanoliverzz@gmail.com>
2024-09-06 22:40:07 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,14 @@
+let dados = [
+  {
+    titulo: "Renan Oliveira",
+    descrição: "Olá me chamo Renan Oliveira, e eu estou estudando programação.",
+    link: "https://www.instagram.com/renan_oliverz/",
+    tags: "renan programacao oliveira insta instagram  Insta Instagram",
+  },
+  {
+    titulo: "GitHub",
+    descrição: "Um pouco mais sobre mim e para quem quer me conhecer melhor!",
+    link: "https://github.com/",
+    tags: " git projeto site GitHub GITHUB github",
+  },
+];
